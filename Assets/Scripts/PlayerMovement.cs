5d9bc984c517ccd429f9f0a26eab13708296c7fb
ShaoKuy <iqtanawad@gmail.com>
2024-03-17 21:33:56 +0700

    Update Script - Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,6 +20,74 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMove();
+        Sprint();
+        Jump();
+    }
+
+    private void PlayerMove()
+    {
+        float moaveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moaveHorizontal, 0f, moveVertical);
+
+        if (movement != Vector3.zero)
+        {
+            if (Input.GetKey(KeyCode.LeftShift) && currentStamita > 0)
+            {
+                rb.velocity = movement * sprintSpeed;
+                currentStamita -= Time.deltaTime * 10f;
+            }
+            else
+            {
+                rb.velocity = movement * walkSpeed;
+            }
+
+            if (currentStamita < staminaMax)
+            {
+                currentStamita += staminaRegenRate;
+                currentStamita = Mathf.Clamp(currentStamita, 0f, staminaMax);
+            }
+        }
+    }
+
+    private void Sprint()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && currentStamita > 0)
+        {
+            walkSpeed = sprintSpeed;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift) || currentStamita <= 0)
+        {
+            walkSpeed = walkSpeed;
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (IsGround())
+            {
+                rb.AddForce(Vector3.up * jumpFore, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private bool IsGround()
+    {
+        RaycastHit hit;
+        float distance = 1f;
+        Vector3 dir = Vector3.down;
+
+        if (Physics.Raycast(transform.position,dir, out hit , distance))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
