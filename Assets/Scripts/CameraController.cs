dc4a11f0c78d7c737dbb5e004129522b3f55c9d4
ShaoKuy <iqtanawad@gmail.com>
2024-03-18 23:02:37 +0700

    Camera System

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float sensitivity;
+    [SerializeField] private Transform playerBody;
+    
+    [SerializeField] private Transform cameraTransform;
+    
+    private float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(cameraTransform);
+        
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        transform.localRotation = Quaternion.Euler(xRotation,0f,0f);
+        playerBody.Rotate(Vector3.up * mouseX );
+        
+        
+    }
+}
