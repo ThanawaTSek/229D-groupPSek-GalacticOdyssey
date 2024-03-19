50c32b3a12294dc9e7249300e69fc7d8be07e078
ShaoKuy <iqtanawad@gmail.com>
2024-03-20 00:38:38 +0700

    Add Script - MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button creditButton;
+    [SerializeField] private Button exitButton; 
+    private void Start()
+    {
+        playButton.onClick.AddListener(PlayGame);
+        creditButton.onClick.AddListener(ShowCredits);
+        exitButton.onClick.AddListener(ExitGame);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+    public void ShowCredits()
+    {
+        SceneManager.LoadScene("EndCredit");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
+}
